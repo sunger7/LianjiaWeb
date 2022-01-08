6df48f797211f7c8487fd8ae224a26bae140af67
sunger7 <sunger7@163.com>
2022-01-08 12:06:09 +0800

    init

---
+++ Webdialog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LianjiaWebWorm
+{
+    public partial class Webdialog : Form
+    {
+        public Webdialog()
+        {
+            InitializeComponent();
+            webBrowser1.Url = new Uri("www.lianjia.com");
+        }
+    }
+}
---
+++ luascript.lua
@@ -0,0 +1,26 @@
+﻿database = sqlitedatabase:new() 
+
+database:filename("D:\\CodeProgram\\LianjiaWebWorm\\bin\\house.db") 
+
+database:connected(true) 
+
+query = sqlitequery:new() 
+
+query:database(database) 
+filter = "where date > '2020.08.01' and date < '2020.09.01'";
+filter1 = "SELECT count(*) FROM "
+query:sql(filter1..  "[main].普陀 " .. filter) 
+query:active(true) 
+print("普陀"..query:getfield(0):value().."\n"); 
+print()
+query:sql(filter1..  "[main].徐汇 " .. filter) 
+query:active(true) 
+print("徐汇"..query:getfield(0):value().."\n"); 
+query:sql(filter1..  "   [main].青浦 " .. filter) 
+query:active(true) 
+print("青浦"..query:getfield(0):value().."\n"); 
+query:sql(filter1..  " [main].长宁 " .. filter) 
+query:active(true) 
+print("长宁"..query:getfield(0):value().."\n"); 
+query:free() 
+database:free() 
